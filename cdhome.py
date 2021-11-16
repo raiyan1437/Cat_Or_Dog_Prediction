6d0be777f7f899b2a46365effe65e30bca99948e
raiyan1437 <82383890+raiyan1437@users.noreply.github.com>
2021-11-16 13:39:02 +0530

    Update cdhome.py

--- cdhome.py
+++ cdhome.py
@@ -4,7 +4,7 @@ import streamlit as st
 def app():
 
      st.title(""" Cat Or Dog Prediction By Raiyan Firaz """)
-     st.image("cat_or_dog.jpg")
+     st.image("dog_pred.jpg")
      st.markdown("""**To predict go to prediction page and uplode an image.**""")
      st.write(""" **Github Repository and Source code is available on About page.** """)
      st.image("cat_or_dog.jpg")
