94216e63e4aadcae8d378ee6bf1025d19966c2be
raiyan1437 <82383890+raiyan1437@users.noreply.github.com>
2021-11-16 11:02:35 +0530

    Add files via upload

---
+++ cdmain.py
@@ -0,0 +1,21 @@
+from model import load_model
+import streamlit as st
+import tensorflow as tf
+from PIL import Image, ImageOps
+import numpy as np
+import keras.preprocessing
+from pages import home, prediction, about
+pages = {
+    "Home": home,
+    "Prediction": prediction,
+    "About": about
+}
+
+
+
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Pages", list(pages.keys()))
+if page == None:
+    home.app()
+else:
+    pages[page].app()
