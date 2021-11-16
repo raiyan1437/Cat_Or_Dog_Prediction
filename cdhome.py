aed8c262936557b3aae05669d091fc2df737067d
raiyan1437 <82383890+raiyan1437@users.noreply.github.com>
2021-11-16 13:43:25 +0530

    Update cdhome.py

--- cdhome.py
+++ cdhome.py
@@ -7,6 +7,6 @@ def app():
      st.image("dog_pred.jpg")
      st.markdown("""**To predict go to prediction page and uplode an image.**""")
      st.write(""" **Github Repository and Source code is available on About page.** """)
-     st.image("cat_or_dog.jpg")
+     st.image("cd.jpg")
 
 
