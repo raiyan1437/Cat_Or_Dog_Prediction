1eff4c56cd870c8f7927c4475ec902f2f8a093a0
raiyan1437 <82383890+raiyan1437@users.noreply.github.com>
2021-11-16 13:53:14 +0530

    Update cdhome.py

--- cdhome.py
+++ cdhome.py
@@ -6,7 +6,7 @@ def app():
      st.title(""" Cat Or Dog Prediction """)
      st.write("** - By Raiyan Firaz **")
      st.markdown("**To predict go to prediction page and uplode an image**")
-     st.image("cd.jpg", width= 500)     
+     st.image("cd.jpg", width= 1000)     
      st.write("Github Repository and Source code is available on About page")
 
 
