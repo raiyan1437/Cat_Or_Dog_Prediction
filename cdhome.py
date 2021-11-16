eaa26044dbf88cd33fac8add2393228dfffc17cc
raiyan1437 <82383890+raiyan1437@users.noreply.github.com>
2021-11-16 13:52:09 +0530

    Update cdhome.py

--- cdhome.py
+++ cdhome.py
@@ -4,10 +4,10 @@ import streamlit as st
 def app():
 
      st.title(""" Cat Or Dog Prediction """)
-     st.write("By Raiyan Firaz")
-     st.image("cd.jpg")
-     st.markdown("**To predict go to prediction page and uplode an image.**")
-     st.write(" **Github Repository and Source code is available on About page.** ")
+     st.write("** - By Raiyan Firaz **")
+     st.markdown("**To predict go to prediction page and uplode an image**")
+     st.image("cd.jpg")     
+     st.write("Github Repository and Source code is available on About page")
 
 
 
